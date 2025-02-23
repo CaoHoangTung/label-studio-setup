e86001beb3667febd9ee098181f3bdc921109d41
An, Tran Cong Viet <work@antran.tech>
2025-02-23 14:40:40 +0700

    v2.3.0: built image

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 
 echo "Installing Label Studio & Convert tool"
 
-docker compose up -d --build
+docker compose up -d
 
 echo "Label Studio setup completed. Please navigate to your browser to continue."
 echo "  - To open the convert tool, go to http://localhost:5000"
@@ -11,3 +11,4 @@ echo "  - To open Label Studio, go to http://localhost:8080"
 echo ""
 echo "Note: The projects are created automatically. Project names are used by convert tool to identify projects.
 Do not rename the projects"
+
